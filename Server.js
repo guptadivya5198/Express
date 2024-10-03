5caaa444c3b6c1538edb9f2a90e682be60c3d2ec
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-03 17:53:09 +0530

    second commit

--- Server.js
+++ Server.js
@@ -4,3 +4,14 @@ const app = express();
 app.listen(3000, () => {
   console.log('server stared at port 3000');
 });
+
+app.get('/', (request, response) => {
+  response.send('Hello World');
+});
+
+app.post('/api/cars', (request, response) => {
+  const { name, brand } = request.body;
+  console.log(name);
+  console.log(brand);
+  response.send('Car Submitted successfully.');
+});
